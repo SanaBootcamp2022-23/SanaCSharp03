a9c7ef238ad7ebc8b8b29ffbfb87654ae420eeea
Andrii Volynets <116294957+AndryVolynets@users.noreply.github.com>
2022-12-23 15:51:25 +0200

    Update Program.cs

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 
 namespace Arrays
 {
@@ -18,15 +18,17 @@ namespace Arrays
 
             for (int i = 0; i < N; i++)
             {
-                arr[i] = GetRandomNumber(-10, 10);
-                Console.Write($"{Math.Round(arr[i], 2)} ");
+                arr[i] = i == random.Next(0, N) ? 
+                    random.Next(-10, 10) :
+                    GetRandomNumber(-10, 10);
+                Console.Write($"{Math.Round(arr[i], 3)}| ");
             }
 
             Console.WriteLine($"\n\n" +
-                              $"Cума від’ємних елементів масиву = {Math.Round(arr.Where(x => x < 0).Sum(), 2)}\n" +
-                              $"Максимальний елемент масиву = {Math.Round(arr.Max())}\n" +
+                              $"Cума від’ємних елементів масиву = {Math.Round(arr.Where(x => x < 0).Sum(), 3)}\n" +
+                              $"Максимальний елемент масиву = {Math.Round(arr.Max(), 3)}\n" +
                               $"Індекс максимального елемента масиву = {arr.ToList().IndexOf(arr.Max())}\n" +
-                              $"Максимальний за модулем елемент масиву = {arr.Max(Math.Abs)}\n" +
+                              $"Максимальний за модулем елемент масиву = {Math.Round(arr.Max(Math.Abs), 3)}\n" +
                               $"Cума індексів додатних елементів = {arr.Select((x, i) => x >= 0 ? i : 0).Sum()}\n" +
                               $"Rількість цілих чисел у масиві = {arr.Count(x => x % 1 == 0)}");
         }
