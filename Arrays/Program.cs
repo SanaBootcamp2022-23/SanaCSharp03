3e0976e1d51ba313ba12c1093f7dd05ef4ca6c33
Rod_Of_Harmony <ipz222_pis@student.ztu.edu.ua>
2022-12-23 15:55:11 +0200

    Task

---
+++ Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿const int n = 10;
+double[] arr = new double[n] { 3.6, 7, 8, 45, 6.5, -9.4, -8, 0, 74, -100 };
+double sum = 0, max, imax = 0;
+foreach(double d in arr)
+    if(d<0)
+    sum += d;
+
+Console.WriteLine($"Sum of negative numbers: {sum}");
+
+max = arr[0];
+for(int i = 1;i< arr.Length;i++)
+    if (max < arr[i])
+    {
+        max = arr[i];
+        imax = i;
+    }
+
+Console.WriteLine($"Max number: {max}");
+Console.WriteLine($"Index of max number: {imax}");
+
+max = Math.Abs(arr[0]);
+for (int i = 1; i < arr.Length; i++)
+    if (max < Math.Abs(arr[i]))
+    max = arr[i];
+
+Console.WriteLine($"Max by module number: {max}");
+sum = 0;
+for (int i = 0; i < arr.Length; i++)
+    if (arr[i] > 0)
+        sum += i;
+
+Console.WriteLine($"Sum of indexes positive numbers: {sum}");
