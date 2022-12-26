c9b1f8a043b2dc2dfc42910ea4fe7fde74458563
sana22mav <sana22_mav@student.ztu.edu.ua>
2022-12-26 02:13:59 +0200

    done

---
+++ Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Write("Введіть кількість елементів масиву: ");
+
+int n = int.Parse(Console.ReadLine());
+double[] array = new double[n];
+
+Console.WriteLine("Введіть елементи масиву:");
+for (int i = 0; i < n; i++)
+{
+    array[i] = double.Parse(Console.ReadLine());
+}
+
+double sumOfNegatives = 0;
+double max = double.MinValue;
+int maxIndex = -1;
+double maxAbs = double.MinValue;
+int sumOfPositiveIndices = 0;
+int countOfIntegers = 0;
+
+for (int i = 0; i < n; i++)
+{
+    if (array[i] < 0)
+    {
+        sumOfNegatives += array[i];
+    }
+    if (array[i] > max)
+    {
+        max = array[i];
+        maxIndex = i;
+    }
+    if (Math.Abs(array[i]) > maxAbs)
+    {
+        maxAbs = Math.Abs(array[i]);
+    }
+    if (array[i] > 0)
+    {
+        sumOfPositiveIndices += i;
+    }
+    if (array[i] == (int)array[i])
+    {
+        countOfIntegers++;
+    }
+}
+
+Console.WriteLine("Сума від'ємних: " + sumOfNegatives);
+Console.WriteLine("Максимальний елемент: " + max);
+Console.WriteLine("Індекс максимального елемента: " + maxIndex);
+Console.WriteLine("Максимальний за модулем елемент: " + maxAbs);
+Console.WriteLine("Сума індексів додатних елементів: " + sumOfPositiveIndices);
+Console.WriteLine("Кількість цілих чисел: " + countOfIntegers);
+Console.ReadLine();
