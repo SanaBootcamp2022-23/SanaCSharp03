fd2e4c6e6f28cdea7d2c085b181801a6ac79f020
Bohdan Serdeniuk <sana22_sbo@student.ztu.edu.ua>
2022-12-29 02:15:06 +0200

    trash to update2 (no need to check)

--- SanaCSharp03/Arrays/Program.cs
+++ SanaCSharp03/Arrays/Program.cs
@@ -2,6 +2,8 @@
 //SanaCShapr03
 //Bohdan Serdeniuk
 
+using System.Linq;
+
 Console.WriteLine("enter number of array elements - ");
 uint numberOfArrayElements = uint.Parse(Console.ReadLine());
 int[] array = new int[numberOfArrayElements];
@@ -11,6 +13,10 @@ int maxValueOfArray;
 uint maxValueArrayIndex = 0;
 int maxValueByModulus = 0;
 uint sumOfIndexWithPositiveValues = 0;
+
+uint countOfMaxValueIndex = 1;
+uint[] arrayOfMaxValueIndex;
+uint n = 0;
 
 //array initialization 
 for (int i = 0; i < numberOfArrayElements; i++)
@@ -27,11 +33,30 @@ for (uint i = 0; i < numberOfArrayElements; i++)
     if (array[i] < 0)
         sumOfNegativeValues += array[i];
 
-    if (array[i] > maxValueOfArray)
+    if (array[i] >= maxValueOfArray)
     {
         maxValueOfArray = array[i];
         maxValueArrayIndex = i;
     }
+
+    for (uint j = maxValueArrayIndex+1; j < numberOfArrayElements ; j++)
+    {
+        if (array[j] == maxValueOfArray)
+        {
+            countOfMaxValueIndex++;
+        }
+    }
+    /*arrayOfMaxValueIndex = new uint[countOfMaxValueIndex];
+    
+    for (uint j = maxValueArrayIndex + 1; j < numberOfArrayElements; j++)
+    {
+        if (array[j] == maxValueOfArray)
+        {   
+            arrayOfMaxValueIndex[n] = j;
+            n++;
+            Console.WriteLine("[{0}]", string.Join(", ", arrayOfMaxValueIndex));
+        }
+    }*/
 
     if (Math.Abs(array[i]) > Math.Abs(maxValueByModulus))
     {
@@ -41,6 +66,19 @@ for (uint i = 0; i < numberOfArrayElements; i++)
     if (array[i] > 0)
         sumOfIndexWithPositiveValues += i;
 }
+
+arrayOfMaxValueIndex = new uint[countOfMaxValueIndex];
+
+for (uint j = maxValueArrayIndex ; j < numberOfArrayElements; j++)
+{
+    if (array[j] == maxValueOfArray)
+    {
+        arrayOfMaxValueIndex[n] = j;
+        n++;
+    }
+}
+Console.WriteLine("[{0}]", string.Join(", ",arrayOfMaxValueIndex));
+
 
 //outputting results
 Console.WriteLine("\nresults:");
@@ -54,6 +92,15 @@ Console.WriteLine($"3. index of the maximum element of the array = {maxValueArra
 Console.WriteLine($"4. maximum element of the array by modulus = {maxValueByModulus}");
 Console.WriteLine($"5. sum of indices of positive elements = {sumOfIndexWithPositiveValues}");
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 //3. подумати про випадок коли 2+ числа мають максимальне значення
+//3. вивести масив індексів значень з максимальним значенням
+/* 
+    створити масив
+    прогнати цикл (від індексу найбільшого елементу; якщо значення елементу == значенню найбільшого елементу; і++)
+    записати в масив значення індексу
+
+    дати на виведення всі значення масиву
+ */
+
 //3. подумати про indexOf
