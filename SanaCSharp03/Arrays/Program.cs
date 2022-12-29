bc905fcc5d85e1f8547dcf44032be314ebe21492
Roman Hrushnyk <sana22_gra@student.ztu.edu.ua>
2022-12-29 21:12:49 +0200

    Create Array

---
+++ SanaCSharp03/Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿double[] arr = new double[2000];
+Random random = new Random();
+
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i % 2 == 0)
+        arr[i] = random.NextDouble() * random.Next(-500, 500);
+    else arr[i] = random.Next(1, 300);
+}
