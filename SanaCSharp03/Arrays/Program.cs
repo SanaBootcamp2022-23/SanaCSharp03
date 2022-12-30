aa0e1ac45179f2172212bba0f8b88339842c2304
MariiaGumenyk <marusik.gum@gmail.com>
2022-12-30 16:04:14 +0200

    fix

--- SanaCSharp03/Arrays/Program.cs
+++ SanaCSharp03/Arrays/Program.cs
@@ -6,7 +6,7 @@ int n;
 Console.Write("Введіть N кількість чисел: ");
 n = int.Parse(Console.ReadLine());
 double[] array = new double[n];
-double sum_dec = 0, max_elem = 0, i_max_elem = 0, max_abc = 0, sum = 0, int_count = 0;
+double sum_dec = 0, max_elem = 0, i_max_elem = 0, max_abc = 0, sum = 0, int_count = 0,max_count=0;
 for (int i = 0; i < n; i++)
 {
     Console.Write("[{0}]: ", i);
@@ -20,6 +20,11 @@ for (int i = 0; i < n; i++)
     {
         max_elem = array[i];
         i_max_elem = i;
+
+        if (max_elem == array[i])
+        {
+            max_count ++;
+        }
     }
 
     if (Math.Abs(max_abc) < Math.Abs(array[i]))
@@ -39,17 +44,29 @@ for (int i = 0; i < n; i++)
     {
         sum += i;
     }
-
+   
 }
+void Max_Elem_Id()
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (max_elem == array[i])
+        {
+           Console.Write("{0} ",i);
+        }
+    }
+}
+
 
 for (int i = 0; i < n; i++)
 {
 
     Console.WriteLine("[{0}]{1}", i, array[i]);
 }
-Console.WriteLine("Cума від’ємних елементів масиву: {0}\n" +
-    "Максимальний елемент масиву: {1}\n" +
-    "Номер (індекс) максимального елемента масиву: {2}\n" +
-    "Максимальний за модулем елемент масиву: {3}\n" +
-    "Сума індексів додатних елементів: {4}\n" +
-    "Кількість цілих чисел у масиві:{5}\n", sum_dec, max_elem, i_max_elem, max_abc, sum, int_count);
+Console.WriteLine("Cума від’ємних елементів масиву: {0}", sum_dec);
+Console.WriteLine("Максимальний елемент масиву: {0}",max_elem);
+Console.Write("Номер (індекс) максимального елемента масиву: ");
+Max_Elem_Id();
+Console.WriteLine("\nМаксимальний за модулем елемент масиву: {0}\n" +
+    "Сума індексів додатних елементів: {1}\n" +
+    "Кількість цілих чисел у масиві:{2}\n", max_abc, sum, int_count);
